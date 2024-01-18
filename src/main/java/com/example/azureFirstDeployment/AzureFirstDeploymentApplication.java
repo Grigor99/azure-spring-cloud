164f1b945fadf1c2f9f106127312cd04c0f266e1
gmartirosyan <grigor.martirosyan@jambit.com>
2024-01-18 17:11:22 +0400

    azure first dep on cloud

---
+++ src/main/java/com/example/azureFirstDeployment/AzureFirstDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.example.azureFirstDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureFirstDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureFirstDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azureFirstDeployment/AzureFirstDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azureFirstDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureFirstDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
